25f9dcc8bbe7aed30af738d7881f5a351d311808
SwonVIP <mail@scheffel.ch>
2019-09-29 10:41:44 +0200

    Adjusted component structure and added sidenav skeleton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { Measurement } from '../app/model/measurement';
-import { MeasurementDataService } from '../app/services/measurement-data.service';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
   title = 'cistern-pwa'
-  measurements: Measurement;
-  interval:any;
-  
-  constructor(private measurementDataService: MeasurementDataService) {
-  }
 
-  //TODO improve by removing interval and adding websocket connection
-  //TODO make interval customizable in settings dialog
-  ngOnInit() {
-      console.log("sdsd");
-      this.getMeasurements();
-      this.interval = setInterval(() => { 
-          this.getMeasurements(); 
-      }, 5000);
-  }
-
-  getMeasurements(): void {
-    console.log("executed");
-    this.measurementDataService.getMeasurements()
-        .subscribe(
-          (measurements) => {
-            this.measurements = measurements;
-          },
-          (err) => console.error(err)
-        );
-  }
 }
---
+++ src/app/sidenav/sidenav.component.ts
@@ -0,0 +1,25 @@
+import {MediaMatcher} from '@angular/cdk/layout';
+import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.scss']
+})
+export class SidenavComponent implements OnDestroy {
+  mobileQuery: MediaQueryList;
+
+  fillerNav = Array.from({length: 50}, (_, i) => `Nav Item ${i + 1}`);
+
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+}
