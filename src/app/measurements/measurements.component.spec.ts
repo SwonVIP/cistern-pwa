c6cb438997689c27e5a96a91012b40f4021dbaba
SwonVIP <mail@scheffel.ch>
2020-04-24 13:14:46 +0200

    Fixed fault tests and async http property dependency

--- src/app/measurements/measurements.component.spec.ts
+++ src/app/measurements/measurements.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { MeasurementsComponent } from './measurements.component';
 
@@ -8,9 +9,10 @@ describe('MeasurementsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MeasurementsComponent ]
+      imports: [HttpClientTestingModule],
+      declarations: [MeasurementsComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
--- src/app/services/measurement-data.service.spec.ts
+++ src/app/services/measurement-data.service.spec.ts
@@ -1,9 +1,11 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { MeasurementDataService } from './measurement-data.service';
 
 describe('MeasurementDataService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule]
+  }));
 
   it('should be created', () => {
     const service: MeasurementDataService = TestBed.get(MeasurementDataService);
