8df1f98f52bf37bfff52124be787dff8c147d31f
SwonVIP <mail@scheffel.ch>
2020-04-27 09:24:34 +0200

    Removed window resolution header

--- e2e/protractor-ci.conf.js
+++ e2e/protractor-ci.conf.js
@@ -3,7 +3,7 @@ const config = require('./protractor.conf').config;
 config.capabilities = {
   browserName: 'chrome',
   chromeOptions: {
-    args: ['--headless', '--disable-gpu', '--no-sandbox', '--window-size=800,600', '--disable-extensions', '--disable-dev-shm-usage']
+    args: ['--headless', '--disable-gpu', '--no-sandbox', '--disable-extensions', '--disable-dev-shm-usage']
   }
 };
 
