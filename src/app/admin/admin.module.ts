c5a34b9d21b07793b8942bd25ed4fb7ea5f4942e
SwonVIP <dev@scheffel.ch>
2021-03-23 11:20:31 +0100

    Fix test dependency issues

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,12 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ApiKeyConfigComponent } from './api-key-config/apiKeyConfig.component';
 import { MatInputModule } from '@angular/material/input';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 
@@ -18,13 +15,9 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
   imports: [
     CommonModule,
     AdminRoutingModule,
-    MatToolbarModule,
     MatInputModule,
-    MatIconModule,
-    MatListModule,
     MatButtonModule,
     MatSnackBarModule,
-    FormsModule,
     ReactiveFormsModule
   ]
 })
--- src/app/admin/api-key-config/apiKeyConfig.component.spec.ts
+++ src/app/admin/api-key-config/apiKeyConfig.component.spec.ts
@@ -1,4 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { AppModule } from 'src/app/app.module';
+import { AdminRoutingModule } from '../admin-routing.module';
+import { AdminModule } from '../admin.module';
 import { ApiKeyConfigComponent } from './apiKeyConfig.component';
 
 describe('ApiKeyConfigComponent', () => {
@@ -7,7 +15,13 @@ describe('ApiKeyConfigComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ApiKeyConfigComponent ]
+      declarations: [ ApiKeyConfigComponent ],
+      // Added Module imports due to test failures
+      // TODO revisit when wiritng Unit tests
+      imports: [
+        AppModule,
+        AdminModule
+      ]
     })
     .compileComponents();
   });
