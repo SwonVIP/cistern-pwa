73b9ea500959c52e5850b9fa426f30b0026c5ac2
SwonVIP <dev@scheffel.ch>
2021-03-24 12:56:31 +0100

    Add test for save config

--- src/app/services/save-config.service.spec.ts
+++ src/app/services/save-config.service.spec.ts
@@ -6,11 +6,40 @@ describe('SaveConfigService', () => {
   let service: SaveConfigService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+    });
     service = TestBed.inject(SaveConfigService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should write data to localStorage', () => {
+    service.saveKeyToStorage("real key");
+  });
+
+  it('should get data from localStorage', () => {
+    service.saveKeyToStorage("real key");
+    let result = service.getExisitingKeyFromStorage()
+    expect(result).toBe("real key");
+  });
+
+  it('should get dummy data key from localStorage', () => {
+    localStorage.removeItem("key");
+    let result = service.getExisitingKeyFromStorage()
+    expect(result).toBe("CC:50:E3:3B:F5:8B");
+  });
+
+  it('should return dummy key true', () => {
+    service.saveKeyToStorage("CC:50:E3:3B:F5:8B");
+    let result = service.isDummyKey();
+    expect(result).toBeTrue
+  })
+
+  it('should return dummy key false', () => {
+    service.saveKeyToStorage("real key");
+    let result = service.isDummyKey();
+    expect(result).toBeFalse
+  })
 });
